21df8386e5ed53d8ebf16b78607f9c06d72ce557
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-03-06 22:44:38 +0530

    now user data will update and generate btn will work

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 const oldImg = document.querySelector("#oldImg");
+const oldName = document.querySelector("#oldName");
+const oldEmail = document.querySelector("#oldEmail");
+const oldPhone = document.querySelector("#oldPhone");
+const oldLocation = document.querySelector("#oldLocation");
+const oldAge = document.querySelector("#oldAge");
+const genBtn = document.querySelector("#generate");
 
-fetch("https://randomuser.me/api/")
-  .then((response) => response.json())
-  .then((data) => {
-    // console.log(data.results[0].picture);
-    oldImg.src = `${data.results[0].picture.large}`;
-  });
+const getDataAndUpdateDOM = () => {
+  fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((data) => {
+      const info = data.results[0];
+
+      // console.log(info);
+      oldImg.src = `${info.picture.large}`;
+
+      oldName.firstChild.replaceWith(
+        document.createTextNode(
+          `Name: ${info.name.title}. ${info.name.first} ${info.name.last}`
+        )
+      );
+
+      oldEmail.firstChild.replaceWith(
+        document.createTextNode(`Email: ${info.email}`)
+      );
+
+      oldPhone.firstChild.replaceWith(
+        document.createTextNode(`Phone: ${info.phone}`)
+      );
+
+      oldEmail.firstChild.replaceWith(
+        document.createTextNode(`Name: ${info.email}`)
+      );
+
+      oldLocation.firstChild.replaceWith(
+        document.createTextNode(
+          `Location: ${info.location.city}, ${info.location.state}, ${info.location.country}`
+        )
+      );
+
+      oldAge.firstChild.replaceWith(
+        document.createTextNode(`Age: ${info.dob.age}`)
+      );
+    });
+};
+
+genBtn.addEventListener("click", getDataAndUpdateDOM);
+
+getDataAndUpdateDOM();
